a011dcbd0ef59d5f5026239c1f312a8a035bd413
EliasR07 <eliasalejo01@gmail.com>
2022-12-18 21:33:09 -0400

    feat: adding pages to src folder

---
+++ src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import "../../styles/globals.css";
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import { ConnectionProvider } from "@solana/wallet-adapter-react";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { ThemeProvider } from "@mui/material";
+import CustomTheme from "lib/theme";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const WalletProvider = dynamic(
+    () => import("../context/ClientWalletProvider"),
+    {
+      ssr: false,
+    }
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider>
+        <ThemeProvider theme={CustomTheme()}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { Box, SxProps } from "@mui/material";
+import { Header } from "components/Home/Header/Header";
+import { LeaderboardContent } from "components/Home/LeaderboardContent/LeaderboardContent";
+
+const headerBoxContainerStyle: SxProps = {
+  backgroundColor: "rgba(217, 217, 217, 0.20)",
+  paddingBottom: "1rem",
+};
+
+const Home = () => {
+  return (
+    <Box sx={headerBoxContainerStyle}>
+      <Header />
+      <LeaderboardContent />
+    </Box>
+  );
+};
+
+export default Home;
