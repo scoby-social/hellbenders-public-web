b9fe133c389bc276644f2246946286e68c135a1c
EliasR07 <eliasalejo01@gmail.com>
2023-01-20 11:33:05 -0400

    fix: Changing clusterApiUrl to env endpoint in places where it was missing

--- src/lib/web3/checkIfUserHasFakeID.ts
+++ src/lib/web3/checkIfUserHasFakeID.ts
@@ -1,10 +1,8 @@
-import { Cluster, clusterApiUrl, Connection } from "@solana/web3.js";
+import { Connection } from "@solana/web3.js";
 import { getNftsForOwner } from "./getNftsForOwner";
 
 export async function checkIfUserHasFakeID(wallet: any): Promise<boolean> {
-  const conn = new Connection(
-    clusterApiUrl(process.env.NEXT_PUBLIC_SOLANA_CLUSTER! as Cluster)
-  );
+  const conn = new Connection(process.env.NEXT_PUBLIC_SOLANA_CLUSTER!);
   const FakeIDNFTSYMBOL = "HELLPASS";
 
   const fakeIDs = await getNftsForOwner(
