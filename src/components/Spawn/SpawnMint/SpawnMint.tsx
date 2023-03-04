171d8aa0c9d47318ae122559fa5a9c0579e27561
EliasR07 <eliasalejo01@gmail.com>
2023-03-04 10:17:52 -0400

    ft: Changing Spawn minting title to Sold Out

--- src/components/Spawn/SpawnMint/SpawnMint.tsx
+++ src/components/Spawn/SpawnMint/SpawnMint.tsx
@@ -82,11 +82,11 @@ const SpawnMint = () => {
   return (
     <Box sx={spawnContainer}>
       <Box sx={spawnTitleWrapper}>
-        <Typography variant="h2">{`Mint your Hellbenders Spawn`}</Typography>
+        <Typography variant="h2">{`We're SOLD OUT!`}</Typography>
         <Typography
           sx={spawnSubtitle}
           variant="subtitle2"
-        >{`In the Available Groups Below`}</Typography>
+        >{`You're too late MFer...`}</Typography>
       </Box>
 
       <Box sx={supplyInfoContainer}>
